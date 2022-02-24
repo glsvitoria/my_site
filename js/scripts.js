374a89863b4dbd15ecab0acf36a0c059e4c08b26
Guilherme Vitória <guivitoria2010@hotmail.com>
2022-02-24 03:31:06 -0300

    Add button backToTop and header fixed

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+/* Mudar o header da página quando der scroll */
+const header = document.querySelector("#header")
+console.log(header)
+const navHeight = header.offsetHeight
+function changeHeaderWhenScroll(){
+    if(window.scrollY >= navHeight){
+        // Scroll é maior que a altura do header
+        header.classList.add('scroll')
+    } else {
+        // Menor que a altura do header
+        header.classList.remove('scroll')
+    }
+}
+
+/* Botão voltar para o topo */
+const backToTopButton = document.querySelector('.back_to_top')
+function backToTop() {
+    if(window.scrollY >= 560){
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
+}
+
+window.addEventListener('scroll', function() {
+    changeHeaderWhenScroll()
+    backToTop()
+})
