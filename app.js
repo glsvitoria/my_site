aa761c332e7d11a837061b5bca048f618fb33b19
Guilherme Vitória <guivitoria2010@hotmail.com>
2022-03-16 01:33:51 -0300

    Put website online in heroku

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path    = require('path')
 const exphbs  = require('express-handlebars')
 const bodyParser = require('body-parser')
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({extended: false}))
 
