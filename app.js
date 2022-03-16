6936808ee78ac679ab836dd506997876e328bbce
Guilherme Vitória <guivitoria2010@hotmail.com>
2022-03-16 01:32:02 -0300

    Create back-end

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const app     = express()
+const path    = require('path')
+const exphbs  = require('express-handlebars')
+const bodyParser = require('body-parser')
+
+const PORT = 3000
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+// HANDLE BARS
+app.set('view engine', 'handlebars')
+const hbs = exphbs.create({defaultLayout: 'main'})
+app.engine('handlebars', hbs.engine)
+app.set('views', path.join(__dirname, 'views'))
+
+// STATIC FOLDER
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(PORT, function(){
+   console.log("O Express está rodando na porta " + PORT)
+})
+
+app.get('/', (req, res) => {
+   res.render('index')
+})
