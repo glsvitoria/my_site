8b99f2bb95add138d8abe0e6b796d2863a20adf3
Guilherme Vitória <guivitoria2010@hotmail.com>
2022-03-19 01:04:08 -0300

    Change Nodejs' image and remove about_image in dark mode

--- public/js/darkMode.js
+++ public/js/darkMode.js
@@ -1,6 +1,13 @@
 const html = document.querySelector('html')
 const checkbox =  document.querySelector('#switch')
+const img = document.querySelector('.about_image')
 
 checkbox.addEventListener('change', () => {
    html.classList.toggle('dark_mode')
+   if(html.classList[0] == 'dark_mode'){
+      img.style.display = "none"
+   }else {
+      img.style.display = "block"
+
+   }
 })
